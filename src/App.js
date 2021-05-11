681bf211b843bbcdea15d3d85950b74699516dc9
pavel-sobchanka <pavel.sobchanka@capsilon.com>
2021-05-11 23:54:30 +0300

    webpack: css modules fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import cn from 'classnames';
+
+import './custom.css';
+import s from './App.modules.scss';
 
 const App = () => {
 	return (
-		<div>
-			This is App Component!
+		<div className={cn(s.header,'color')}>
+			This is App Component Marathon!
 		</div>
 	)
 }
