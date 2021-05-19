b23c621c68c8df2c556de5859e76a09d59c3216c
pavel-sobchanka <pavel.sobchanka@capsilon.com>
2021-05-20 00:29:41 +0300

    fix: added dynamic pokemon card rendering

--- src/components/PokemonCard/index.tsx
+++ src/components/PokemonCard/index.tsx
@@ -2,34 +2,35 @@ import React from 'react';
 import Heading from "../Heading";
 
 import s from './PokemonCard.module.scss';
+import { IPokemonDescription } from '../../models/IPokemonDescription';
 
-const PokemonCard = (props: any) => {
+const PokemonCard: React.FC<IPokemonDescription> = (item) => {
     return (
       <div className={s.root}>
           <div className={s.infoWrap}>
               <Heading scale={'h1'}>
-                  Charmander
+                  {item.name_clean}
               </Heading>
               <div className={s.statWrap}>
                   <div className={s.statItem}>
                       <div className={s.statValue}>
-                          52
+                          {item.stats.attack}
                       </div>
                       Attack
                   </div>
                   <div className={s.statItem}>
                       <div className={s.statValue}>
-                          43
+                          {item.stats.defense}
                       </div>
                       Defense
                   </div>
               </div>
               <div className={s.labelWrap}>
-                  <span className={s.label}>Fire</span>
+                  <span className={s.label}>{item.types.join(',')}</span>
               </div>
           </div>
           <div className={s.pictureWrap}>
-              <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png" alt="Charmander" />
+              <img src={item.img} alt={item.name_clean} />
           </div>
       </div>
     );
--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { EmptyPageProps } from '../../components/EmptyPage';
 import PokemonCard from '../../components/PokemonCard';
 import { pokemons } from '../../models/pokemons';
+import { IPokemonDescription } from '../../models/IPokemonDescription';
+import s from './Pokedex.module.scss';
 
-const Pokedex: React.FC<EmptyPageProps> = ({title}) => {
+const Pokedex: React.FC<EmptyPageProps> = () => {
      const pokemonItems = pokemons.map(
-       (item) => <PokemonCard key={item.name_clean} value={item} />
+       (item: IPokemonDescription) =>
+         <PokemonCard key={item.id} {...item}/>
        );
      return(
-       <div>
+       <div className={s.root}>
            {pokemonItems}
        </div>
      );
