8f6d77c39f8af340024eeee7236e637f58d20393
pavel-sobchanka <pavel.sobchanka@capsilon.com>
2021-06-02 18:11:52 +0300

    fix: SSR the very first commit

--- src/components/PokemonCard/PokemonSimpleCard.tsx
+++ src/components/PokemonCard/PokemonSimpleCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Heading from "../Heading";
-import { A, navigate, usePath } from 'hookrouter';
+import { A, navigate, usePath, useQueryParams } from 'hookrouter';
 
 import s from './PokemonCard.module.scss';
 import { IPokemonDescription } from '../../models/IPokemonDescription';
@@ -9,7 +9,8 @@ import { LinkEnum } from '../../../routes';
 export interface IPokemonSimple {
     name: string;
     url: string;
-    index: number;
+    index?: number | undefined;
+    key?: string;
 }
 
 const PokemonSimpleCard: React.FC<IPokemonSimple> = (item) => {
@@ -21,7 +22,7 @@ const PokemonSimpleCard: React.FC<IPokemonSimple> = (item) => {
                       {item.name[0].toUpperCase() + item.name.slice(1)}
                   </Heading>
               </div>
-              <div onClick={() => navigate(`/pokedex/${item.index}`)}>
+              <div onClick={() => navigate(`/pokedex/${item.index}`,[], [item])}>
                  <img className={s.pictureWrap_simple} alt="pokemon" src={`https://img.pokemondb.net/artwork/large/${item.name}.jpg`}/>
               </div>
           </div>
--- src/pages/Pokemon/index.tsx
+++ src/pages/Pokemon/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import StandaloneCard from '../../components/PokemonCard/StandaloneCard';
+import {useQueryParams } from 'hookrouter';
 
 export interface PokemonProps {
     id: string | number;
 }
 
 const Pokemon: React.FC<PokemonProps> = ({id}) => {
+    const [queryParams] = useQueryParams();
+    const q = queryParams[0];
     return (
       <div>
-          This is pokemon
+          <div>
+              This is pokemon {id}
+          </div>
+          <StandaloneCard index={q.index} name={q.name} url={q.url}/>
       </div>
     );
 };
